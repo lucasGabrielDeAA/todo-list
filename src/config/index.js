ab77c2d7789cbb990ac3292fd329c6e765022574
lucasGabrielDeAA <lg.technus@gmail.com>
2018-04-17 14:48:50 -0300

    Config directory added

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyC3Umfxkz-TJovOoFxUXirpbGnc6aufUmY",
+    authDomain: "myproject-ef2f6.firebaseapp.com",
+    databaseURL: "https://myproject-ef2f6.firebaseio.com",
+    projectId: "myproject-ef2f6",
+    storageBucket: "myproject-ef2f6.appspot.com",
+    messagingSenderId: "34686710715"
+};
